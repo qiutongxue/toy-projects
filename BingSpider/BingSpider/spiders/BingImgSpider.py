f8f5dbf0b90367b0877b090e046e29504b110289
qiutongxue <53330358+qiutongxue@users.noreply.github.com>
2020-09-19 16:35:09 +0800

    修复下载图片分辨率问题

--- BingSpider/BingSpider/spiders/BingImgSpider.py
+++ BingSpider/BingSpider/spiders/BingImgSpider.py
@@ -7,23 +7,28 @@ class BingImgSpider(scrapy.Spider):
 
     name = 'BingImgSpider'
     allowed_domains = ['bing.ioliu.cn']
-    start_urls = ['https://bing.ioliu.cn/?p=' + str(_) for _ in range(50, 100)]
+    # start_urls = ['https://bing.ioliu.cn/?p=' + str(_) for _ in range(50, 100)]
+
+    start_urls = ['https://bing.ioliu.cn']
 
     def parse(self, response):
         selectors = response.css(".container .item");
-        # print(selectors)
-        for selector in selectors:
-            time.sleep(1)
-            item = BingspiderItem()
-            imgurl = selector.css("img::attr(src)").extract_first()
-            imgtitle = selector.css(".description h3::text").extract_first()
-            item['img_title'] = imgtitle
-            item['img_url'] = imgurl
-            yield item
-        # imgurls = response.css(".container img::attr(src)").extract()
-        # imgtitles = response.css(".container .description h3::text").extract()
-        # print(imgtitles)
-        # item['img_title'] = imgtitles
-        # item['img_url'] = imgurls
-        # yield item
+        
+        # for selector in selectors:
+        #     time.sleep(1)
+        #     item = BingspiderItem()
+        #     imgurl = selector.css("img::attr(src)").extract_first()
+        #     imgtitle = selector.css(".description h3::text").extract_first()
+        #     item['img_title'] = imgtitle
+        #     item['img_url'] = imgurl
+        #     yield item
+        
+        item = BingspiderItem()
+        imgurl = selectors.css("img::attr(src)").extract_first()
+        imgtitle = selectors.css(".description h3::text").extract_first()
+        item['img_title'] = imgtitle
+        pre_url = imgurl.split('?')
+        item['img_url'] = imgurl.split('?')[0].replace('640x480', '1920x1080')
+        yield item
+
         pass
